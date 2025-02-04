777bd54b94aac83d310b6e33d03bee1659427f7b
quan <quan_xiaoxiao@qq.com>
2025-02-04 17:43:20 +0800

    xxx

--- src/getResourceUrl.mjs
+++ src/getResourceUrl.mjs
@@ -1,24 +1,24 @@
 import assert from 'node:assert';
+import fs from 'node:fs';
 
 import {
+  getValueOfPathList,
   hasDataKey,
+  template,
 } from '@quanxiaoxiao/utils';
 import chalk from 'chalk';
 import shelljs from 'shelljs';
-import { $ } from 'zx';
 
-import getResourceUrl from './getResourceUrl.mjs';
 import resources from './resources.mjs';
 
-export default async (type, configPathname) => {
+export default (type, configPathname) => {
   if (!shelljs.test('-f', configPathname)) {
     console.log(`config \`${chalk.red(configPathname)}\` not found`);
     process.exit(1);
   }
+  let config = JSON.parse(fs.readFileSync(configPathname));
   assert(hasDataKey(resources, type));
-  const requestUrl = getResourceUrl(type, configPathname);
-  const resultWithRequest = await $`curl -s ${requestUrl}`;
-  const data = JSON.parse(resultWithRequest).data;
-  assert(data);
-  return data;
+  const requestUrl = template(getValueOfPathList(resources[type].pathname)(config))(config);
+  assert(requestUrl);
+  return requestUrl;
 };
